a07e9b63330d24ffd1b0a92d2cc08ab01892ad29
SebastianL2 <sebas16cely@gmail.com>
2024-02-26 17:22:54 -0500

    desc

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,24 @@
 import { Badge } from "flowbite-react";
 import { BsGithub } from "react-icons/bs";
-
+import { SiFastapi } from "react-icons/si";
 const Footer = () => {
   return (
     <footer className="flex flex-col items-center justify-center w-full p-24">
-      <p className="text-lg text-center mb-4">Created By</p>
+        <p className="text-lg text-center mb-4">FAST API DOCUMENTATION HERE</p>
+      <div className="flex flex-wrap gap-2">
+       
+        <Badge size="sm" href="https://global-locations.vercel.app/docs" icon={SiFastapi}>
+          Documentacion Fast Api 
+        </Badge>
+      </div>
+      <p className="text-lg text-center mb-4">By</p>
       <div className="flex flex-wrap gap-2">
        
         <Badge size="sm" href="https://github.com/SebastianL2" icon={BsGithub}>
-          Sebastian Cely
+          Sebastian Cely: Documentacion Fast Api 
         </Badge>
       </div>
+
     </footer>
   );
 };
