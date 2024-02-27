ccf0a814037b7233ec9c371ef982088a737ceed2
SebastianL2 <sebas16cely@gmail.com>
2024-02-26 22:46:43 -0500

    jiji

--- src/services/zipcode/zipcode.ts
+++ src/services/zipcode/zipcode.ts
@@ -3,18 +3,17 @@ import { API_URL } from "../../../utils/constans";
 
 export const getZipcode = async (
   zipcode: string
-): Promise<string  | any> => {
-  
+): Promise<ResponseZipCode | any> => {
   try {
     const res = await fetch(`${API_URL}/zipcode/${zipcode}`);
-    const data= await res.json();
+    const data: ResponseZipCode = await res.json();
     return data;
   } catch (error: Error | any) {
     return error.message;
   }
 };
 
-export const getZipcodeByFile = async (file: File): Promise<any> => {
+export const getZipcodeByFile = async (file: File) => {
   try {
     const formData = new FormData();
     formData.append("file", file);
@@ -22,11 +21,9 @@ export const getZipcodeByFile = async (file: File): Promise<any> => {
       method: "POST",
       body: formData,
     });
-    if (!res.ok) {
-      throw new Error(`Error al enviar el archivo: ${res.statusText}`);
-    }
-    return await res.json();
-  } catch (error) {
-    throw new Error(`Error en la solicitud: ${error}`);
+    const data = await res.json();
+    return data;
+  } catch (error: Error | any) {
+    return error.message;
   }
 };
